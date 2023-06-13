1074587946bcba69990904572fa644d46dcb31c3
tarsil <tiago.arasilva@gmail.com>
2023-06-13 09:50:57 +0100

    🪲 Typo in M2M docs example

--- docs_src/queries/manytomany/no_rel_query_example.py
+++ docs_src/queries/manytomany/no_rel_query_example.py
@@ -4,8 +4,8 @@ green_team = await Team.query.create(name="Green Team")
 
 # Add the teams to the organisation
 organisation = await Organisation.query.create(ident="Acme Ltd")
-organisation.teams.add(blue_team)
-organisation.teams.add(green_team)
+await organisation.teams.add(blue_team)
+await organisation.teams.add(green_team)
 
 # Query
-blue_team.team_organisationteams_set.filter(name=blue_team.name)
+await blue_team.team_organisationteams_set.filter(name=blue_team.name)
