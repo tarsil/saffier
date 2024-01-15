8f4d8842d23f483725c099c9284a687a88bb787d
tarsil <tiago.arasilva@gmail.com>
2024-01-15 10:00:03 +0000

    Add default for SETTINGS_MODULE

--- saffier/conf/__init__.py
+++ saffier/conf/__init__.py
@@ -1,1 +1,7 @@
-from dymmond_settings import settings as settings
+import os
+
+try:
+    from dymmond_settings import settings as settings
+except ModuleNotFoundError:
+    os.environ.setdefault("SETTINGS_MODULE", "saffier.conf.global_settings.SaffierSettings")
+    from dymmond_settings import settings as settings
