85e69f7687df1504d9558774dcff4d6da9014941
alex <devkral@web.de>
2024-08-07 10:28:48 +0200

    Changes:
    - really adapt Databasez >= 0.8

--- tests/cli/utils.py
+++ tests/cli/utils.py
@@ -6,8 +6,11 @@ def run_cmd(app, cmd, is_app=True):
     env = dict(os.environ)
     if is_app:
         env["SAFFIER_DEFAULT_APP"] = app
+    # CI uses something different as workdir and we aren't hatch test yet.
     if "VIRTUAL_ENV" not in env:
-        cmd = f"{env['PWD']}/venv/bin/{cmd}"
+        basedir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+        if os.path.isdir(f"{basedir}/venv/bin/"):
+            cmd = f"{basedir}/venv/bin/{cmd}"
     result = subprocess.run(cmd, capture_output=True, env=env, shell=True)
     print("\n$ " + cmd)
     print(result.stdout.decode("utf-8"))
