f4331fafc66cda8a170871006c69eb16c6a2ea2f
alex <devkral@web.de>
2024-08-07 04:28:41 +0200

    CI fix

--- tests/cli/utils.py
+++ tests/cli/utils.py
@@ -6,7 +6,8 @@ def run_cmd(app, cmd, is_app=True):
     env = dict(os.environ)
     if is_app:
         env["SAFFIER_DEFAULT_APP"] = app
-    cmd = f"{env['PWD']}/venv/bin/{cmd}"
+    if "VIRTUAL_ENV" not in env:
+        cmd = f"{env['PWD']}/venv/bin/{cmd}"
     result = subprocess.run(cmd, capture_output=True, env=env, shell=True)
     print("\n$ " + cmd)
     print(result.stdout.decode("utf-8"))
